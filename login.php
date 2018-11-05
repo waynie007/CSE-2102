7a96d9c77586ebe7602bdcf96641a869a81951bf
Ajay Dindayal <sodesh21@gmail.com>
2018-11-05 11:59:47 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- <title>Registration system PHP and MySQL</title> --> 
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="header">
+		<h1> Oasis Database </h1> 
+			<br>
+		<h2>Login</h2>
+	</div>
+	
+	<form method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			New Member? <a href="register.php">Sign Up!</a>
+		</p>
+	</form>
+
+
+</body>
+</html>
